be3b520e17ea106b2062a581d33c0126e79dd5bb
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-02-26 17:07:46 +0100

    texto botones, boton salir

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//const arrayImagenes = [document.getElementById("imagenpiedra"), document.getElementById("imagenpapel"), document.getElementById("imagentijera"), document.getElementById("imagenlagarto"), document.getElementById("imagenspock")]
+// Array con las imagenes para la funcion aleatoria
 const pathsList = ['Lagarto', 'Papel', 'Piedra', 'Spock', 'Tijeras']
-
+// Funcion para elegir una imagen aleatoriamente cada vez que llames a la funcion
 let result = aleatoria()
 function aleatoria() {
     return Math.floor(Math.random() * pathsList.length)
@@ -10,8 +10,8 @@ function aleatoria() {
 let estadoImagenMaquina
 let estadoImagenUsuario
 
+// Dentro de esta funcion hemos dado valor a la variable estadoImagenMaquina, y será el mismo valor cada vez que la utilices en el codigo.
 function CreateImageMachine() {
-    // dentro de esta funcion hemos dado valor a la variable estadoImagenMaquina, y será el mismo valor cada vez que la utilices en el codigo.
     estadoImagenMaquina = (pathsList[aleatoria()])
     const machineTurn = document.createElement('img')
     machineTurn.setAttribute('src', `imagenes/${estadoImagenMaquina}.png`)
@@ -19,10 +19,10 @@ function CreateImageMachine() {
     const machineContainer = document.querySelector('#imagenOpcionPC')
 
 
-    //Variable que recoje la imagen de la maquina tras el primer click
+    //Variable que recojerá la imagen de la maquina tras el primer click
     let viejo = document.getElementById("machineTurn")
 
-    // Condicion para mostrar o no la opcion del PC
+    // Condicion para mostrar o no la imagen de opcion del PC
     if (viejo) {
         //Esto es para sustituir la imagen vieja por la nueva
         machineContainer.replaceChild(machineTurn, viejo)
@@ -33,67 +33,74 @@ function CreateImageMachine() {
     }
 }
 
+// condiciones del juego con mensaje de navegador
 function mensajes() {
 
     if (estadoImagenMaquina === estadoImagenUsuario) {
-        console.log("Empate");
         return "Empate"
     }
     else if (estadoImagenUsuario === 'Lagarto' && estadoImagenMaquina === 'Tijeras' || estadoImagenUsuario === 'Tijeras' && estadoImagenMaquina === 'Lagarto') {
-        console.log("Tijeras decapitan a lagarto")
-        return "Tijeras decapitan a lagarto"
+
+        return "Tijera decapitan a lagarto"
     }
     else if (estadoImagenUsuario === 'Papel' && estadoImagenMaquina === 'Tijeras' || estadoImagenUsuario === 'Tijeras' && estadoImagenMaquina === 'Papel') {
-        console.log("Tijeras cortan papel")
-        return "Tijeras cortan papel"
+
+        return "Tijera cortan papel"
     }
     else if (estadoImagenUsuario === 'Spock' && estadoImagenMaquina === 'Papel' || estadoImagenUsuario === 'Papel' && estadoImagenMaquina === 'Spock') {
-        console.log("Papel desacredita a Spock")
         return "Papel desautoriza a Spock"
     }
     else if (estadoImagenUsuario === 'Piedra' && estadoImagenMaquina === 'Papel' || estadoImagenUsuario === 'Papel' && estadoImagenMaquina === 'Piedra') {
-        console.log("Papel envuelve a piedra")
         return "Papel envuelve a piedra"
     }
     else if (estadoImagenUsuario === 'Tijeras' && estadoImagenMaquina === 'Piedra' || estadoImagenUsuario === 'Piedra' && estadoImagenMaquina === 'Tijeras') {
-        console.log("Piedra aplasta tijeras")
-        return "Piedra aplasta a tijeras"
+
+        return "Piedra aplasta a tijera"
     }
     else if (estadoImagenUsuario === 'Lagarto' && estadoImagenMaquina === 'Piedra' || estadoImagenUsuario === 'Piedra' && estadoImagenMaquina === 'Lagarto') {
-        console.log("Piedra aplasta a lagarto")
+
         return "Piedra aplasta a lagarto"
     }
     else if (estadoImagenUsuario === 'Papel' && estadoImagenMaquina === 'Lagarto' || estadoImagenUsuario === 'Lagarto' && estadoImagenMaquina === 'Papel') {
-        console.log("Lagarto devora papel")
+
         return "Lagarto devora a papel"
     }
     else if (estadoImagenUsuario === 'Spock' && estadoImagenMaquina === 'Lagarto' || estadoImagenUsuario === 'Lagarto' && estadoImagenMaquina === 'Spock') {
-        console.log("Lagarto envenena a Spock")
+
         return "Lagarto envenena a Spock"
     }
     else if (estadoImagenUsuario === 'Tijeras' && estadoImagenMaquina === 'Spock' || estadoImagenUsuario === 'Spock' && estadoImagenMaquina === 'Tijeras') {
-        console.log("Spock rompe tijeras")
-        return "Spock desintegra tijeras"
+
+        return "Spock desintegra tijera"
     }
     else if (estadoImagenUsuario === 'Piedra' && estadoImagenMaquina === 'Spock' || estadoImagenUsuario === 'Spock' && estadoImagenMaquina === 'Piedra') {
-        console.log("Spock vaporiza a piedra")
+
         return "Spock vaporiza a Piedra"
     }
     else {
-        console.log("No has pulsado correctamente")
+
         return "No has pulsado correctamente"
     }
 }
 
-function mensajesNavegador (){
+// Funcion que añade el mensaje de la funcion If else en el navegador,
+function mensajesNavegador() {
     const tagMensajes = document.createElement('p');
-    tagMensajes.textContent =`${mensajes()}`;
+    tagMensajes.setAttribute('id', "tagMensajes")
+    tagMensajes.textContent = `${mensajes()}`;
     const mensajesContainer = document.getElementById("contenedorMensajes");
-    mensajesContainer.appendChild(tagMensajes)
+
+    let MensajeAnterior = document.getElementById("tagMensajes")
+// Aqui añado condicion para que el navegador añada el mensaje, o reemplace por uno nuevo si hay uno anterior
+    if (MensajeAnterior) {
+        mensajesContainer.replaceChild(tagMensajes, MensajeAnterior)
+    } else {
+        mensajesContainer.appendChild(tagMensajes)
+    }
 }
 
 
-
+// Funcion inicial desde donde empieza el juego, contiene el evento, y la llamada a todas las funciones que queremos que ejecute con ese evento. 
 function selectOption() {
     //const pc = document.getElementById("imagenOpcionPC")
     const piedra = document.getElementById("piedra")
@@ -106,8 +113,7 @@ function selectOption() {
         lagarto.style.backgroundColor = "#69727984"
         spock.style.backgroundColor = "#69727984"
         CreateImageMachine()
-        mensajes()
-        mensajesNavegador ()
+        mensajesNavegador()
 
 
     })
@@ -121,8 +127,7 @@ function selectOption() {
         lagarto.style.backgroundColor = "#69727984"
         spock.style.backgroundColor = "#69727984"
         CreateImageMachine()
-        mensajes()
-        mensajesNavegador ()
+        mensajesNavegador()
 
     })
 
@@ -135,8 +140,7 @@ function selectOption() {
         lagarto.style.backgroundColor = "#69727984"
         spock.style.backgroundColor = "#69727984"
         CreateImageMachine()
-        mensajes()
-        mensajesNavegador ()
+        mensajesNavegador()
 
     })
     const lagarto = document.getElementById("lagarto")
@@ -148,8 +152,7 @@ function selectOption() {
         tijera.style.backgroundColor = "#69727984"
         spock.style.backgroundColor = "#69727984"
         CreateImageMachine()
-        mensajes()
-        mensajesNavegador ()
+        mensajesNavegador()
 
     })
     const spock = document.getElementById("spock")
@@ -161,8 +164,7 @@ function selectOption() {
         papel.style.backgroundColor = "#69727984"
         piedra.style.backgroundColor = "#69727984"
         CreateImageMachine()
-        mensajes()
-        mensajesNavegador ()
+        mensajesNavegador()
     })
 }
 
